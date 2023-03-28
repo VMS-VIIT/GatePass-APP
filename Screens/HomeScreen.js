9802f4e411d747af1cd94ffbb28e44e6f33f44c3
sid120 <siddharthrakshe12@gmail.com>
2023-03-28 23:15:36 +0530

    created home page

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,65 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { View, Text, Button } from "react-native";
 
 export default function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text
+        onPress={() => alert('This is the "Home" screen.')}
+        style={{ fontSize: 26, fontWeight: "bold" }}
+      >
+        Home Screen
+      </Text>
+      <button
+        style={{
+          fontSize: 26,
+          fontWeight: "bold",
+          backgroundColor: "#FFBF00",
+          borderRadius: 50,
+          marginTop: 20,
+          width: 200,
+          height: 40,
+          borderRadius: 5,
+          position: "relative",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Entry
+      </button>
+      <button
+      style={{
+        fontSize: 26,
+        fontWeight: "bold",
+        backgroundColor: "#FFBF00",
+        borderRadius: 50,
+        marginTop: 20,
+        width: 200,
+        height: 40,
+        borderRadius: 5,
+        position: "relative",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Exit
+    </button> <button
+    style={{
+      fontSize: 26,
+      fontWeight: "bold",
+      backgroundColor: "#FFBF00",
+      borderRadius: 50,
+      marginTop: 20,
+      width: 200,
+      height: 40,
+      borderRadius: 5,
+      position: "relative",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    Visitor Details 
+  </button>
+    </View>
+  );
 }
